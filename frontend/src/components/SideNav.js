9e6acc66e4a80e0763cacdc6ff86dc42a98761c5
Aswin A <iamtheaswin@gmail.com>
2024-07-18 22:57:28 +0530

    .

--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
   BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill,
@@ -7,6 +7,7 @@ import {
 import '../assets/css/SideNav.css'; // Adjust the path as per your project structure
 
 const SideNav = ({ setLoggedInUser }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -14,46 +15,52 @@ const SideNav = ({ setLoggedInUser }) => {
     navigate('/login');
   };
 
-  return (
-    <aside id="sidebar">
-      <div className='sidebar-title'>
-        <div className='sidebar-brand'>
-          <BsGrid1X2Fill className='icon_header' /> Bluestone
-        </div>
-        <span className='icon close_icon'>X</span>
-      </div>
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
-      <ul className='sidebar-list'>
+  return (
+    <>
+      <BsGrid1X2Fill className='toggle-icon' onClick={toggleSidebar} />
+      <aside id="sidebar" className={isOpen ? 'open' : ''}>
+        <div className='sidebar-title'>
+          <div className='sidebar-brand'>
+            <BsGrid1X2Fill className='icon_header' /> Bluestone
+          </div>
+          <span className='icon close_icon' onClick={toggleSidebar}>X</span>
+        </div>
+        <ul className='sidebar-list'>
           <Link to="/user/dashboard" className="sidebar-link">
-        <li className='sidebar-list-item'>
-            <BsGrid1X2Fill className='icon' /> Dashboard
-        </li>
+            <li className='sidebar-list-item'>
+              <BsGrid1X2Fill className='icon' /> Dashboard
+            </li>
           </Link>
           <Link to="/enquiryform" className="sidebar-link">
-        <li className='sidebar-list-item'>
-            <BsFillArchiveFill className='icon' /> Enquiry Form
-        </li>
+            <li className='sidebar-list-item'>
+              <BsFillArchiveFill className='icon' /> Enquiry Form
+            </li>
           </Link>
-        <li className='sidebar-list-item'>
-          <Link to="/about" className="sidebar-link">
-            <BsFillGrid3X3GapFill className='icon' /> About
-          </Link>
-        </li>
-        <li className='sidebar-list-item'>
-          <Link to="/contact" className="sidebar-link">
-            <BsPeopleFill className='icon' /> Contact
-          </Link>
-        </li>
-        <li className='sidebar-list-item'>
-          <Link to="/settings" className="sidebar-link">
-            <BsFillGearFill className='icon' /> Settings
-          </Link>
-        </li>
-        <li className='sidebar-list-item' onClick={handleLogout}>
-          <BsMenuButtonWideFill className='icon' /> Logout
-        </li>
-      </ul>
-    </aside>
+          <li className='sidebar-list-item'>
+            <Link to="/about" className="sidebar-link">
+              <BsFillGrid3X3GapFill className='icon' /> About
+            </Link>
+          </li>
+          <li className='sidebar-list-item'>
+            <Link to="/contact" className="sidebar-link">
+              <BsPeopleFill className='icon' /> Contact
+            </Link>
+          </li>
+          <li className='sidebar-list-item'>
+            <Link to="/settings" className="sidebar-link">
+              <BsFillGearFill className='icon' /> Settings
+            </Link>
+          </li>
+          <li className='sidebar-list-item' onClick={handleLogout}>
+            <BsMenuButtonWideFill className='icon' /> Logout
+          </li>
+        </ul>
+      </aside>
+    </>
   );
 };
 
